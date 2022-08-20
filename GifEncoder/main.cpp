47ac5ecdd5992bcd5d19ac90ea2bd0367f33c616
robmikh <rob.mikh@outlook.com>
2022-08-19 23:32:14 -0700

    Add project files.

---
+++ GifEncoder/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+
+namespace winrt
+{
+    using namespace Windows::Foundation;
+    using namespace Windows::Foundation::Numerics;
+    using namespace Windows::UI;
+    using namespace Windows::UI::Composition;
+}
+
+namespace util
+{
+    using namespace robmikh::common::desktop;
+}
+
+int __stdcall wmain()
+{
+    // Initialize COM
+    winrt::init_apartment(winrt::apartment_type::multi_threaded);
+
+    // 
+
+    return 0;
+}
